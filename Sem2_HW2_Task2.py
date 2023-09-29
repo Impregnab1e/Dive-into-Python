2f728dcf71b8f7dec1dd53de1e44d39cdda5602c
Dmitriy <wuwka1@ya.ru>
2023-09-29 17:07:44 +0300

    HW2 update 2

--- Sem2_HW2_Task2.py
+++ Sem2_HW2_Task2.py
@@ -1,5 +1,6 @@
-# Напишите программу, которая принимает две строки вида “a/b” - дробь с числителем и знаменателем. 
-# Программа должна возвращать сумму и произведение* дробей. 
+# Напишите программу, которая принимает две строки вида “a/b”
+# - дробь с числителем и знаменателем.
+# Программа должна возвращать сумму и произведение* дробей.
 # Для проверки своего кода используйте модуль fractions.
 
 from fractions import Fraction
@@ -14,11 +15,11 @@ sum_numerator = (numerator1 * denominator2) + (numerator2 * denominator1)
 product_numerator = numerator1 * numerator2
 product_denominator = denominator1 * denominator2
 
-print(f"Сумма без fractions): {sum_numerator}/{denominator1 * denominator2}")
-print(f"Произведение без fractions): {product_numerator}/{product_denominator}")
+print(f"Сумма без fractions: {sum_numerator}/{denominator1 * denominator2}")
+print(f"Произведение без fractions: {product_numerator}/{product_denominator}")
 
 fraction1 = Fraction(numerator1, denominator1)
 fraction2 = Fraction(numerator2, denominator2)
 
-print(f"Сумма с fractions): {fraction1 + fraction2}")
-print(f"Произведение с fractions): {fraction1 * fraction2}")
+print(f"Сумма с fractions: {fraction1 + fraction2}")
+print(f"Произведение с fractions: {fraction1 * fraction2}")
